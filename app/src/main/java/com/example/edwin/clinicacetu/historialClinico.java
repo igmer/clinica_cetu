90a1a031db577cb76c499bf504101f1bb857832d
rodriguez <igmerdz@gmail.com>
2018-05-09 14:52:08 -0600

    en trabajo

--- app/src/main/java/com/example/edwin/clinicacetu/historialClinico.java
+++ app/src/main/java/com/example/edwin/clinicacetu/historialClinico.java
@@ -9,5 +9,6 @@ public class historialClinico extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_clinico);
+
     }
 }
