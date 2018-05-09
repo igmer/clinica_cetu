8398a34d0c54193351e1a6e72bd3b8afd7af9180
rodriguez <igmerdz@gmail.com>
2018-05-09 16:11:31 -0600

    ya

--- app/src/main/java/com/example/edwin/clinicacetu/ClinicaDB.java
+++ app/src/main/java/com/example/edwin/clinicacetu/ClinicaDB.java
@@ -18,7 +18,7 @@ public class ClinicaDB extends SQLiteOpenHelper{
     String sqlCreateTableUsuarios = "Create Table Usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT,"
             + "nombre TEXT, direccion TEXT, telefono TEXT,FechaNac DATETIME DEFAULT CURRENT_TIMESTAM, email TEXT, usuario TEXT, password TEXT, activo INTEGER)";
 
-    String sqlCretaeConsultaMedica="create table consultaMedica(id INTEGER PRIMARY KEY AUTOINCREMENT, idCita integer NOT NULL, peso DOUBLE, talla DOUBLE, diagnostico TEXT, tratamiento TEXT)";
+    String sqlCretaeConsultaMedica="create table consultaMedica (id INTEGER PRIMARY KEY AUTOINCREMENT, idCita integer NOT NULL, peso DOUBLE, talla DOUBLE, diagnostico TEXT, tratamiento TEXT)";
 
 
     public ClinicaDB(Context context) {
@@ -49,6 +49,8 @@ public class ClinicaDB extends SQLiteOpenHelper{
         db.execSQL("INSERT INTO Usuarios VALUES (1,'','','','1998-01-01 10:00:00','albertolemus87@gmai.com','elemus','123',1)");
         db.execSQL("INSERT INTO Usuarios VALUES (2,'','','','1998-01-01 10:00:00','arcana_87@hotmail.com','juan','1234x',1)");
         db.execSQL("INSERT INTO Usuarios VALUES (4,'','','','1998-01-01 10:00:00','aguardado@nexuserp.coom','carlos','456',1)");
+        db.execSQL("INSERT INTO consultaMedica VALUES (1,1,3.1,10.3,'Diarrea','tapacul')");
+
 
 
     }
--- app/src/main/java/com/example/edwin/clinicacetu/historialClinico.java
+++ app/src/main/java/com/example/edwin/clinicacetu/historialClinico.java
@@ -1,18 +1,35 @@
 package com.example.edwin.clinicacetu;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class historialClinico extends AppCompatActivity {
-
+    ListView lwHistoral;
+    ArrayList listado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_clinico);
+        lwHistoral = (ListView)findViewById(R.id.lwHistorial);
+        cargarListado();
+
+    }
+    private void cargarListado(){
+        listado = listaPersonas();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,listado);
+        lwHistoral.setAdapter(adapter);
 
     }
 
@@ -24,7 +41,7 @@ public class historialClinico extends AppCompatActivity {
         Cursor c = db.rawQuery(historial, null);
         if (c.moveToFirst()){
             do {
-                String linea  = c.getInt(0)+" - "+c.getString(1)+" - "+c.getString(2);
+                String linea  = c.getInt(0)+" - "+c.getString(4)+" - "+c.getString(5);
                 datos.add(linea);
             }while(c.moveToNext());
         }
@@ -32,5 +49,38 @@ public class historialClinico extends AppCompatActivity {
         return datos;
 
 
+    }
+    public class MyAdapter extends BaseAdapter{
+        private Context context;
+        private  int layout;
+        private List<String> names;
+
+        public MyAdapter(Context context, int layout, List<String> names){
+
+
+        }
+
+        @Override
+        public int getCount() {
+            return this.names.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return this.names.get(position);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            View v = convertView;
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+            v= layoutInflater.inflate(R.layout.list_item, names);
+            return ;
+        }
     }
 }
